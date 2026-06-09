a575ddd62ecf87cbce3504ce44eb9e83aafcaabd
Dhruv Chaudhary <dhruv.chaudhary240307@gmail.com>
2026-06-09 21:16:39 +0530

    Day 9 completed

---
+++ Day 9/Q35.java
@@ -0,0 +1,13 @@
+public class Q35 {
+    public static void main(String[] args) {
+
+        for (char ch = 'A'; ch <= 'E'; ch++) {
+
+            for (char j = 'A'; j <= ch; j++) {
+                System.out.print(ch);
+            }
+
+            System.out.println();
+        }
+    }
+}
