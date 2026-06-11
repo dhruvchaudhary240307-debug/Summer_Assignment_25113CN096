76ad4cd3cf189e06beef713e4b49773805f38f08
Dhruv Chaudhary <dhruv.chaudhary240307@gmail.com>
2026-06-11 21:01:10 +0530

    Day 11 completed

---
+++ Day 11/Q44.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Q44 {
+
+    static long factorial(int n) {
+
+        long fact = 1;
+
+        for (int i = 1; i <= n; i++) {
+            fact *= i;
+        }
+
+        return fact;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number: ");
+        int n = sc.nextInt();
+
+        System.out.println("Factorial = " + factorial(n));
+
+        sc.close();
+    }
+}
