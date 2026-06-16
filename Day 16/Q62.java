91bbc159d27c5eeceb76c0e0bcdfc8b212f9260d
Dhruv Chaudhary <dhruv.chaudhary240307@gmail.com>
2026-06-16 09:48:41 +0530

    Day 16 completed

---
+++ Day 16/Q62.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Q62 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size: ");
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+
+        System.out.println("Enter elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int maxFreq = 0;
+        int element = arr[0];
+
+        for (int i = 0; i < n; i++) {
+            int count = 0;
+
+            for (int j = 0; j < n; j++) {
+                if (arr[i] == arr[j]) {
+                    count++;
+                }
+            }
+
+            if (count > maxFreq) {
+                maxFreq = count;
+                element = arr[i];
+            }
+        }
+
+        System.out.println("Maximum Frequency Element = " + element);
+
+        sc.close();
+    }
+}
