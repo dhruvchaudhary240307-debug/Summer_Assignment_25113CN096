752d9e2c02839bfb48b2b15829c4a6636b1d317f
Dhruv Chaudhary <dhruv.chaudhary240307@gmail.com>
2026-06-26 21:07:38 +0530

    Day 25 completed

---
+++ Day 25/Q98.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Q98 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first string: ");
+        String s1 = sc.nextLine();
+
+        System.out.print("Enter second string: ");
+        String s2 = sc.nextLine();
+
+        System.out.print("Common Characters: ");
+
+        for (int i = 0; i < s1.length(); i++) {
+
+            char ch = s1.charAt(i);
+
+            if (s2.indexOf(ch) != -1) {
+                System.out.print(ch + " ");
+            }
+        }
+
+        sc.close();
+    }
+}
